1678f8391904b9804635d0f4fdc2f5c9006222f9
Chahu <2451370340@qq.com>
2022-10-04 22:36:48 +0800

    First Triangle

--- XuanWu/src/Platform/OpenGL/OpenGLContext.cpp
+++ XuanWu/src/Platform/OpenGL/OpenGLContext.cpp
@@ -17,6 +17,11 @@ namespace XuanWu {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		XW_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		XW_CORE_INFO("OpenGL Info:");
+		XW_CORE_INFO("Vendor: {0}", glGetString(GL_VENDOR));
+		XW_CORE_INFO("Renderer: {0}", glGetString(GL_RENDERER));
+		XW_CORE_INFO("Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
